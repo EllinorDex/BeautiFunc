5ebf7295091804e96dca0f27ee6507ab92611401
ellinordex <ellinordex@gmail.com>
2021-09-20 11:56:59 +0300

    Чтение из БД на C++ и создание структуры классов

---
+++ praktika/praktika/Argument.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <string>
+
+class Argument {
+public:
+	Argument()
+	{
+		name = "";
+		typeName = "";
+		baseTypeName = "";
+		kind = "";
+		order = -1;
+	}
+	Argument(const Argument& arg)
+	{
+		name = arg.name;
+		typeName = arg.typeName;
+		baseTypeName = arg.baseTypeName;
+		kind = arg.kind;
+		order = arg.order;
+	}
+
+	Argument& operator = (const Argument& arg)
+	{
+		if (this != &arg)
+		{
+			name = arg.name;
+			typeName = arg.typeName;
+			baseTypeName = arg.baseTypeName;
+			kind = arg.kind;
+			order = arg.order;
+		}
+		return *this;
+	}
+
+	bool operator < (const Argument& arg) { return order < arg.order; }
+	/*
+	std::string GetName() const { return name; }
+	std::string GetTypeName() const { return typeName; }
+	std::string GetBaseTypeName() const { return baseTypeName; }
+	std::string GetKind() const { return kind; }
+	int GetOrder() const { return order; }
+
+	void SetName(std::string Name) { name = Name; }
+	void SetTypeName(std::string TypeName) { typeName = TypeName; }
+	void SetBaseTypeName(std::string BaseTypeName) { baseTypeName = BaseTypeName; }
+	void SetKind(std::string Kind) { kind = Kind; }
+	void SetOrder(int Order) { order = Order; }*/
+
+	std::string name;
+	std::string typeName;
+	std::string baseTypeName;
+	std::string kind;
+	int order;
+};
---
+++ praktika/praktika/praktika.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include "sqlite/sqlite3.h"
+#include <string>
+#include "Function.h"
+#include <stdlib.h>
+
+
+/////////////////////////////////////////                                                               struct result
+
+
+/////////////////////////////////////////                                                              DLLS callback
+static int DAR_callback(void* result, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++)
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    ((Function*)result)->DLL = argv[1] ? argv[1] : "";
+    ((Function*)result)->API_Categorie = argv[2] ? argv[2] : "";
+    ((Function*)result)->Return_Type = argv[3] ? argv[3] : "";
+    return 0;
+}
+static int Check_function_callback(void* result, int argc, char** argv, char** azColName)
+{
+    for (int i = 0; i < argc; i++)
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    if (atoi(argv[0]) > 0)
+        return 0;
+    return 1;
+}
+
+static int ARG_callback(void* result, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++)
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    Argument arg;
+    arg.baseTypeName = argv[5] ? argv[5] : "";
+    arg.kind = argv[4] ? argv[4] : "";
+    arg.name = argv[2] ? argv[2] : "";
+    arg.order = argv[1] ? atoi(argv[1]) : -1;
+    arg.typeName = argv[3] ? argv[3] : "";
+    ((Function*)result)->Arguments.push_back(arg);
+    return 0;
+}
+
+
+
+/////////////////////////////////////////                                                               extract_from_db
+Function extract_from_db(std::string name)
+{
+    Function function;
+    function.Name = name;
+    sqlite3* db;
+    char* zErrMsg = 0;
+    int rc;
+    std::string sql;
+
+    /* Open database */
+    rc = sqlite3_open("ApiMonitor.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(function);
+    }
+    else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    sql = "SELECT count(*) FROM Functions WHERE func_name = \"" + name + "\";";
+    rc = sqlite3_exec(db, sql.c_str(), Check_function_callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+        sqlite3_close(db);
+        return function;
+    }
+    else {
+        fprintf(stdout, "Operation done successfully\n");
+
+    }
+
+    /* Create SQL statement */
+    sql = "SELECT func_name, dll_name, api_name, type_name AS ret_type_name FROM \
+        ((SELECT * FROM Functions LEFT JOIN API_Categories ON Functions.api_id = API_Categories.api_id) AS FA\
+            LEFT JOIN DLLs ON DLLs.dll_id = FA.dll_id) AS FAD LEFT JOIN Types ON FAD.ret_type_id = Types.type_id\
+        WHERE func_name = \"";
+    sql += (name + "\";");
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql.c_str(), DAR_callback, &function, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    sql = "SELECT func_name, arg_order, arg_name, type_name, kind, base_name FROM\
+    (SELECT * FROM Functions LEFT JOIN Arguments ON Functions.func_id = Arguments.func_id) AS FA\
+        LEFT JOIN Types ON Types.type_id = FA.arg_type_id\
+        WHERE func_name = \"";
+    sql += (name + "\";");
+    rc = sqlite3_exec(db, sql.c_str(), ARG_callback, &function, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    sqlite3_close(db);
+    return function;
+}
+
+
+
+
+/////////////////////////////////////////                                                               main
+
+int main(int argc, char* argv[]) {
+    Function function;
+    std::string name = "DebugCreate";
+    function = extract_from_db(name);
+    std::cout << function.API_Categorie << std::endl;
+    //std::cout << function.Arguments.front().order << std::endl;
+    return 0;
+}
